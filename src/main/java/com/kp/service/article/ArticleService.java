906288e699f7689d0b27dbb306a9b6386ddc9693
Turgay Can <turgay.can@yandex.com.tr>
2016-09-12 01:52:28 +0300

    add cache clear method!

--- src/main/java/com/kp/service/article/ArticleService.java
+++ src/main/java/com/kp/service/article/ArticleService.java
@@ -12,7 +12,6 @@ import org.apache.commons.collections4.ListUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
